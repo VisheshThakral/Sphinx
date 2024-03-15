a9c1a7ce5b04e41a858198c78e77cf6d6bacf1ee
Vishesh Thakral <thakralvishesh@gmail.com>
2024-03-15 16:05:45 +0530

    Ui improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { User } from './models/user.model';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sphinx';
+  currentUser: User;
+  private subscription: Subscription;
+
+  constructor(
+    private authService: AuthService,
+  ) {
+    this.subscription = this.authService.currentUser.subscribe((user) => {
+      this.currentUser = user;
+    });
+  }
 }
